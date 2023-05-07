518010c21eddb3885d5c44ec579d13258ba3f39c
Mikail Doumi <mdoumi@k2r4p3.42mulhouse.fr>
2023-05-07 13:49:57 +0200

    3d

--- src/raycasting.c
+++ src/raycasting.c
@@ -14,9 +14,10 @@
 # define PI 3.1415926535
 # define P2 PI/2
 # define P3 3*PI/2
+# define DR 0.0174533
 
-void drawRays3D ();
-void	draw_line(int x0, int y0, int x1, int y1);
+void drawRays2D();
+void	draw_line(int x0, int y0, int x1, int y1, int color);
 
 mlx_t	*mlx;
 mlx_image_t	*plyr_img;
@@ -26,11 +27,11 @@ int	mapX=8, mapY=8, mapS=64;
 int	map[]=
 {
 	1,1,1,1,1,1,1,1,
-	1,0,1,0,0,0,0,1,
-	1,0,1,0,0,0,0,1,
-	1,0,1,0,0,0,0,1,
 	1,0,0,0,0,0,0,1,
-	1,0,0,0,0,1,0,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,1,
 	1,0,0,0,0,0,0,1,
 	1,1,1,1,1,1,1,1,
 };
@@ -54,16 +55,17 @@ float dist (float ax, float ay, float bx, float by)
 	return (sqrt((bx-ax) * (bx-ax) + (by-ay) * (by-ay)) );
 }
 
-void drawRays3D ()
+void drawRays2D ()
 {
-	int r,mx, my, mp, dof; float rx, ry, ra, xo, yo;
-	ra=pa;
-	for (r=0;r<1;r++)
+	int r,mx, my, mp, dof; float rx, ry, ra, xo, yo, disT;
+	ra=pa-DR*30; if(ra<0){ ra+=2*PI;} if(ra>2*PI){ ra -=2*PI;}
+	for (r=0;r<60;r++)
 	{
 		//---Check Horizontal Lines---
 		dof=0;
 		float disH = 1000000,hx=px,hy=py;
 		float aTan=-1/tan(ra);
+		int	color;
 		if (ra>PI) { ry=(((int) py>>6) <<6) -0.0001;	rx= (py-ry)*aTan+px; yo=-64; xo=-yo*aTan;}
 		if (ra<PI) { ry=(((int) py>>6) <<6)+64;			rx= (py-ry)*aTan+px; yo= 64; xo=-yo*aTan;}
 		if (ra==0 || ra==PI) { rx=px; ry=py; dof=8; }//looking straight left or right
@@ -84,16 +86,29 @@ void drawRays3D ()
 		while (dof<8)
 		{
 			mx = (int) (rx) >> 6; my= (int)(ry)>>6; mp=my*mapX+mx;
-			if (mp > 0 && mp<mapX*mapY && map [mp]==1){vx=rx;vy=ry;disV=dist(px,py,vx,vy); dof=8; }//hit wall
+			if (mp > 0 && mp<mapX*mapY && map[mp]==1){vx=rx;vy=ry;disV=dist(px,py,vx,vy); dof=8; }//hit wall
 			else{ rx+=xo; ry+=yo; dof+=1; }//next line
 		}
-		if(disV<disH){rx=vx;ry=vy;}
-		if(disV>disH){rx=hx;ry=hy;}
-		draw_line(px + 4, py + 4, rx, ry);
+		if(disV<disH){rx=vx;ry=vy; disT=disV; if (ra > P2 && ra < P3) {color = BLU;} else {color = YEL;}} //Vertical lines
+		if(disV>disH){rx=hx;ry=hy; disT=disH; if (ra > PI) {color = RED;} else {color = PIN;}} //Horizontal lines
+
+		//---Draw 3D Walls---
+		float ca=pa-ra; if(ca<0) {ca+=2*PI;} if(ca>2*PI) {ca-=2*PI;} disT=disT*cos(ca);
+		float lineH=(mapS*320)/disT; if(lineH>320){lineH=320;}
+		float lineO=160-lineH/2;
+		for (int z = 0; z < 8; z++)
+		{
+			draw_line(r*8+530+z, 0, r*8+530+z, lineO, CYA); // Plafond
+			draw_line(r*8+530+z, lineO, r*8+530+z, lineH+lineO, color); // Mur
+			draw_line(r*8+530+z, lineH+lineO, r*8+530+z, 512, GRE); // Sol
+		}
+
+		draw_line(px + 4, py + 4, rx, ry, RED);
+		ra+=DR; if(ra<0){ ra+=2*PI;} if(ra>2*PI){ ra -=2*PI;}
 	}
 }
 
-void	draw_line(int x0, int y0, int x1, int y1)
+void	draw_line(int x0, int y0, int x1, int y1, int color)
 {
 	int dx = ft_abs(x1-x0), sx = x0<x1 ? 1 : -1;
 	int dy = ft_abs(y1-y0), sy = y0<y1 ? 1 : -1;
@@ -102,7 +117,7 @@ void	draw_line(int x0, int y0, int x1, int y1)
 	for(;;)
 	{
 		if (x0 >= 0 && x0 <= 1024 && y0 >= 0 && y0 <= 512)
-			mlx_put_pixel(dir_img, x0, y0, RED);
+			mlx_put_pixel(dir_img, x0, y0, color);
 		else
 			return ;
 		if (x0==x1 && y0==y1) break;
@@ -118,7 +133,7 @@ void	mouse(void *param)
 	int	x, y;
 
 	mlx_get_mouse_pos(mlx, &x, &y);
-	draw_line(100, 100, x, y);
+	//draw_line(100, 100, x, y);
 }
 
 mlx_image_t	*fill(mlx_t *mlx, int w, int h, uint32_t color)
@@ -166,7 +181,7 @@ void	key_hook(mlx_key_data_t keydata, void *param)
 	//Draw direction line
 	mlx_delete_image(mlx, dir_img);
 	dir_img = mlx_new_image(mlx, 1024, 512);
-	drawRays3D();
+	drawRays2D();
 	mlx_image_to_window(mlx, dir_img, 0, 0);
 }
 
@@ -178,6 +193,7 @@ void	draw_player(void *param)
 	plyr_img = fill(mlx, 8, 8, YEL);
 	mlx_image_to_window(mlx, plyr_img, px, py);
 
+	draw_line(px + 4, py + 4, px+pdx*5+4, py+pdy*5+4, YEL);
 }
 
 void	display()
