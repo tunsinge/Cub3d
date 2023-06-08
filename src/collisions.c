ae2942e726de98f364d839eae15920fd1489cc45
Mikail Doumi <mdoumi@k2r3p7.42mulhouse.fr>
2023-06-08 14:30:18 +0200

    caca

--- src/collisions.c
+++ src/collisions.c
@@ -6,7 +6,7 @@
 /*   By: mdoumi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 20:41:42 by mdoumi            #+#    #+#             */
-/*   Updated: 2023/06/08 13:13:17 by mdoumi           ###   ########.fr       */
+/*   Updated: 2023/06/08 14:12:29 by mdoumi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	move_x(t_cub3d *uwu, float dx)
 	float	nb;
 
 	nb = uwu->px + dx * uwu->speed;
-	if (uwu->map[(int)uwu->py / uwu->map_s][(int)nb / uwu->map_s] == '1')
+	if ((int)nb / uwu->map_s < 0 || (int)nb / uwu->map_s >= uwu->map_s_x
+		|| uwu->py / uwu->map_s < 0 || uwu->py / uwu->map_s >= uwu->map_s_y)
+		uwu->py += dx * uwu->speed;
+	else if (uwu->map[(int)uwu->py / uwu->map_s][(int)nb / uwu->map_s] == '1')
 		return ;
 	else if (uwu->map[(int)uwu->py / uwu->map_s][(int)nb / uwu->map_s] == '4')
 		return ;
@@ -29,7 +32,10 @@ void	move_y(t_cub3d *uwu, float dy)
 	float	nb;
 
 	nb = uwu->py + dy * uwu->speed;
-	if (uwu->map[(int)nb / uwu->map_s][(int)uwu->px / uwu->map_s] == '1')
+	if ((int)nb / uwu->map_s < 0 || (int)nb / uwu->map_s >= uwu->map_s_y
+		|| uwu->px / uwu->map_s < 0 || uwu->px / uwu->map_s >= uwu->map_s_x)
+		uwu->py += dy * uwu->speed;
+	else if (uwu->map[(int)nb / uwu->map_s][(int)uwu->px / uwu->map_s] == '1')
 		return ;
 	else if (uwu->map[(int)nb / uwu->map_s][(int)uwu->px / uwu->map_s] == '4')
 		return ;
